e8281c899a72e4b3a38ad7ac2197f02aeae51e13
Quent-ger <quent.ger.design@gmail.com>
2025-06-20 12:03:16 +0200

    ajout valeur attribut min  pour date de fin

--- js/photokit-form.js
+++ js/photokit-form.js
@@ -5,33 +5,36 @@
             
         const reservationEndDate = document.getElementById('pk_reservation_end_date');
 
-
-        reservationStartDate.addEventListener('change', function() { // regarde l'évènement changement sur le champ du formulaire
+// regarde l'évènement changement sur le champ du formulaire
+        reservationStartDate.addEventListener('change', function() { 
             // Récupération de la valeur de l'élèment HTML date de début (pk_start... etc)
+
                 const startDateString = reservationStartDate.value;
 
-            // On transforme la date de départ en objet date pour luis appliquer une méthode plus tard    
+
+            // On transforme la date de départ en objet date pour luis appliquer une méthode plus tard   
+             
                 const startDateObj = new Date(startDateString);
 
+
             // Création d'une nouvelle date pour l'objet date de fin pour éviter de faire une référence
+
                 let reservationEndDateObj=new Date(startDateObj);
                 // la méthode pour modifier, ici on ajoute 7 jours
                 reservationEndDateObj.setDate(reservationEndDateObj.getDate() + 7);
+
 
             // création d'une variable dédiée à la mise au bon format de l'objet date de fin vers une chaîne de caractères
                 const tempEndString=reservationEndDateObj.toISOString();
                 let reservationEndString = tempEndString.slice(0,10);
 
+
             // on assigne la valeur de la chaîne de caractères à la valeur de l'élèment HTML date de fin
                 reservationEndDate.value = reservationEndString;
+                
 
-                console.log("Date de début sélectionnée :", startDateObj);
-
-                console.log(reservationEndDate.value);
-
-        
-
-
+            // on l'assigne également à l'attribut min pour l'effet de coloration en gris sur le calendrier    
+                reservationEndDate.min = reservationEndString;
 
         });
     });
